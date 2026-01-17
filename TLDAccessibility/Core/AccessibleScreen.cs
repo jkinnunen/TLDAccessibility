1a291f513e3139e7400c7996611df71b758adb92
Jani Kinnunen <janikinnunen340@gmail.com>
2026-01-17 06:29:48 +0200

    Add UI scanning and narration

---
+++ TLDAccessibility/Core/AccessibleScreen.cs
@@ -0,0 +1,9 @@
+namespace TLDAccessibility.Core;
+
+public sealed class AccessibleScreen
+{
+    public string ScreenId { get; init; } = "UnknownScreen";
+    public string Title { get; init; } = string.Empty;
+    public DateTimeOffset Timestamp { get; init; } = DateTimeOffset.UtcNow;
+    public AccessibleElement? FocusedElement { get; init; }
+}

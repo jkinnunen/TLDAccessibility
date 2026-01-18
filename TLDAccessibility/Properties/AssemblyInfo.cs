779c2afd427afbef239a42c56a664eb9fcb2a61e
Jani Kinnunen <janikinnunen340@gmail.com>
2026-01-18 08:22:16 +0200

    Add MelonLoader assembly metadata

---
+++ TLDAccessibility/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using TLDAccessibility;
+
+[assembly: MelonInfo(typeof(TLDAccessibilityMod), "TLDAccessibility", "1.0.0", "jkinnunen")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]

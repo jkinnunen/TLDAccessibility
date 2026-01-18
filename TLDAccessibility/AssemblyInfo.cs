4b87874e77d784a3d22f12a787040f4f5e4ba28e
Jani Kinnunen <janikinnunen340@gmail.com>
2026-01-18 10:08:24 +0200

    Remove duplicate MelonLoader attributes

--- TLDAccessibility/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using MelonLoader;
-
-[assembly: MelonInfo(typeof(TLDAccessibility.TLDAccessibilityMod), "TLDAccessibility", "1.0.0", "jkinnunen")]
-[assembly: MelonGame("Hinterland", "TheLongDark")]
